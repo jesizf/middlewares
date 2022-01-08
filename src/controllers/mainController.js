aef09726e4c9c3e8586897f7391fca2100dc4c5a
jesica <jesicaguerrerof@gmail.com>
2022-01-07 23:27:50 -0300

    done

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,5 +1,8 @@
 module.exports = {
     index : (req, res) => res.render('index'),
     login : (req, res) => res.render('login'),
+    contact : (req, res) => res.render('contact'),
+    about: (req, res) => res.render('about'),
+    music: (req, res) => res.render('music'),
     admin : (req, res) => res.render('admin', {user: req.query.user})
 }
--- src/routes/main.js
+++ src/routes/main.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
-const{index, login, admin} = require('../controllers/mainController');
+const{index, login, admin, contact, about, music} = require('../controllers/mainController');
 const userlogs = require('../middlewares/userLogs');
 const auth = require('../middlewares/authAdmin');
 router.get('/', index)
 router.get('/login', login)
 router.get('/admin', auth, admin)
-
+router.get('/contact', contact)
+router.get('/about', about)
+router.get('/music', music)
 module.exports = router;
 
 
